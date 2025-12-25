86090bc1d648ed3bd83212ae93ba096667e05715
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2025-12-25 16:39:28 +0530

    pagess

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
-import FeaturedSection from '../components/FeaturedSection'
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturedSection from "../components/FeaturedSection";
+import TrailersSection from "../components/TrailersSection";
 
-const Home = ()=>{
+const Home = () => {
   return (
     <>
-      <HeroSection/>
-      <FeaturedSection/>
+      <HeroSection />
+      <FeaturedSection />
+      <TrailersSection />
     </>
-  )
-}
-export default Home
+  );
+};
+
+export default Home;
