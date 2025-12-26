08184acf685f3ec7fbcf8abf5aab93f540626c40
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2025-12-26 18:21:50 +0530

    some bugs and App.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-  if (!PUBLISHABLE_KEY) {
-    throw new Error('Add your Clerk Publishable Key to the .env file')
-  }
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ClerkProvider>,
-  
-)
+  </ClerkProvider>
+);
