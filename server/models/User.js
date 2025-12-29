b13c431e0540aae8f25f828913b3871fb9a14f12
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2025-12-29 15:57:24 +0530

    backend first commit

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema =  new mongoose.Schema(
+    {
+        _id : {
+            type: String,
+            required : true,
+            unique: true,
+            lowercase : true,
+            trim : true,
+            index: true, // for searching in database , agar har jagah kar diya to performance gir jaati     
+        },
+        email : {
+            type: String,
+            required : true,
+            unique: true,
+            lowercase : true,
+            trim : true,
+        },
+        name : {
+            type: String,
+            required : true,
+            trim : true,
+            index: true,
+        },
+        image:{
+            type: String,
+            required:true,
+             // cloudinary url
+        },
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const User = mongoose.model('User',userSchema)
